9d70b592b15c63c2886aa7122839ef64878a6ed6
sagar-aryal <sagar_yal92@outlook.com>
2022-08-04 23:26:59 +0300

    dist file removed

--- api/dist/util/squareNumber.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function squareNumber(n) {
-    return n * n;
-}
-exports.default = squareNumber;
-//# sourceMappingURL=squareNumber.js.map
